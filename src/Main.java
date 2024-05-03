89d67a013ab660ad6d58972820b12d217c5b7ec6
tutin-zzz <velazquezagustin705@gmail.com>
2024-05-03 00:42:40 -0300

    detalle de pelicula

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Bienvenidos a la inmersión Java");
+    public static void main(String[] orgs) {
+        System.out.println("Bienvenidos a la inmersión en Java");
         System.out.println("pelicula Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        double evaluacion = 4.5;
+        boolean incluidoEnElPlanBasico = true;
+        String nombre = "Matrix";
+        String sinopsis = """
+                La mejor pelicula del fin del milenio
+                """;
+        System.out.println("pelicula: " + nombre);
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(evaluacion);
+        System.out.println(incluidoEnElPlanBasico);
     }
 }
