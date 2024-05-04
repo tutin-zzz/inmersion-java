ea6ee6a0002c84e69e1aa12541466ea3cbaa6d88
tutin-zzz <velazquezagustin705@gmail.com>
2024-05-03 23:48:52 -0300

    valoracion de usuario

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] orgs) {
@@ -11,9 +12,30 @@ public class Main {
         String sinopsis = """
                 La mejor pelicula del fin del milenio
                 """;
+        double mediaEvalucionesUsuario = 0;
+
+
         System.out.println("pelicula: " + nombre);
         System.out.println(fechaDeLanzamiento);
         System.out.println(evaluacion);
         System.out.println(incluidoEnElPlanBasico);
+
+        double mediaEvaluacion = (4.5 + 4.8 + 3) / 3;
+        System.out.println("Media de la evaluacion de Matrix: " + mediaEvaluacion);
+
+        if (fechaDeLanzamiento > 2023){
+            System.out.println("Pelicula popular del momento");
+        } else {
+            System.out.println("Pelicula retro que vale la pena");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("ingresa la puntuacion sobre Matrix");
+            double notaMatrix = teclado.nextDouble();
+            mediaEvalucionesUsuario = mediaEvalucionesUsuario + notaMatrix;
+        }
+        System.out.println("La media de la pelicula Matrix es: " + mediaEvaluacion / 3);
+
     }
 }
